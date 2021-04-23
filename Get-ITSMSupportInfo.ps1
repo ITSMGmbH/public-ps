a01a50b38bcf4ed55eb94270c7bae1d50afdc9c6
Marco Hahnen <marco.hahnen@itsm.de>
2021-04-23 18:22:06 +0200

    init

--- Get-ITSMSupportInfo.ps1
+++ Get-ITSMSupportInfo.ps1
@@ -3,29 +3,26 @@ $DiagLogName = $env:USERPROFILE + "\Desktop\ITSM-SupportInfoLog-$env:computernam
 $WarningPreference = "SilentlyContinue"
 
 Write-Host "Please Wait..."
-Write-Host "." -NoNewline
+Write-Host "."
 quser | out-file -Append -FilePath $DiagLogName 
-Write-Host "." -NoNewline
 ipconfig /all | out-file -Append -FilePath $DiagLogName 
-Write-Host "." -NoNewline
 route print | out-file -Append -FilePath $DiagLogName 
-Write-Host "." -NoNewline
 $NetIPConfiguration = Get-NetIPConfiguration
-Write-Host "." -NoNewline
+
 $dnsservers = ($NetIPConfiguration | Select-Object -ExpandProperty DNSServer).ServerAddresses | select -Unique
 foreach ($dnsserver in $dnsservers) {
     Write-Host "." -NoNewline
     Test-NetConnection $dnsserver | fl | out-file -Append -FilePath $DiagLogName 
     Resolve-DnsName -Name vpn.itsm.de -Server $address | out-file -Append -FilePath $DiagLogName 
 }
-Write-Host "." -NoNewline
+
 $Gateways = ($NetIPConfiguration | select -ExpandProperty IPV4DefaultGateway).NextHop
-Write-Host "." -NoNewline
 foreach($Gateway in $Gateways)
 {
     Write-Host "." -NoNewline
     Test-NetConnection $Gateway | fl | out-file -Append -FilePath $DiagLogName 
 }
+
 Write-Host "." -NoNewline
 Test-NetConnection vpn.itsm.de -Port 443  | fl | out-file -Append -FilePath $DiagLogName
 Write-Host "." -NoNewline
@@ -36,6 +33,7 @@ Write-Host "." -NoNewline
 Test-NetConnection google.de -TraceRoute | fl | out-file -Append -FilePath $DiagLogName 
 Write-Host "." -NoNewline
 Test-NetConnection 8.8.8.8 -TraceRoute | fl | out-file -Append -FilePath $DiagLogName 
-Write-Host "." -NoNewline
+
 systeminfo | out-file -Append -FilePath $DiagLogName 
-Write-Host "Finished"
+
+Write-Host "Finished. Log written to $DiagLogName"
