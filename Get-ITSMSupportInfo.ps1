ae57ef19a3972f15fa540cab1cfa354f52c5515e
Marc Nonn <marc.nonn@ITSM.de>
2022-09-23 11:50:26 +0200

    disable reg export

--- Get-ITSMSupportInfo.ps1
+++ Get-ITSMSupportInfo.ps1
@@ -788,7 +788,7 @@ AppendReport -content ($recentEvents | Select-Object TimeCreated, Id, LevelDispl
 
 Write-Debug "Copying Forticlient Logs"
 Copy-ForticlientLogs
-Copy-ForticlientConfig
+#Copy-ForticlientConfig
 
 Write-Debug "Copying Centerdevice Logs"
 Copy-CenterdeviceLogs
